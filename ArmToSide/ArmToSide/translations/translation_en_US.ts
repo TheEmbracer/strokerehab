746121a38893409152cd8290fc15ac4230927275
TheEmbracer <39994982+TheEmbracer@users.noreply.github.com>
2018-08-02 11:54:23 +0100

    Complete ArmToSide Exercise

---
+++ ArmToSide/ArmToSide/translations/translation_en_US.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Instructions</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Raise your weaker arm out to the side as high as you can but not higher than your shoulder, for a count of 3. Then lower your arm down for a count of 3. Touch my hand to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Raise your weaker arm out to the side as high as you can but not higher than your shoulder, for a count of 3. Then lower your arm down for a count of 3. Touch my hand to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Introduce Exercise</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>This exercise is called the arm to side exercise.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">This exercise is called the arm to side exercise.</translation>
+        </message>
+    </context>
+</TS>
