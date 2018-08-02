9d308004c29571fb01ac94571c425ef27457ec39
TheEmbracer <39994982+TheEmbracer@users.noreply.github.com>
2018-08-02 12:04:12 +0100

    Third version of project put together

---
+++ DemoStudyProject/DemoStudyProject.pml
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="DemoStudyProject" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="index" src="html/index.html" />
+        <File name="one" src="html/one.jpg" />
+        <File name="three" src="html/three.jpg" />
+        <File name="two" src="html/two.jpg" />
+        <File name="zero" src="html/zero.jpg" />
+        <File name="ArmToSide" src="html/ArmToSide.gif" />
+        <File name="TotalArmStretch" src="html/TotalArmStretch.gif" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ DemoStudyProject/translations/translation_en_US.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/ArmToSide/Instructions</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Raise your weaker arm out to the side as high as you can but not higher than your shoulder, for a count of 3. Then lower your arm down for a count of 3. Touch my hand to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Raise your weaker arm out to the side as high as you can but not higher than your shoulder, for a count of 3. Then lower your arm down for a count of 3. Touch my hand to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/ArmToSide/Introduce Exercise</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>This exercise is called the arm to side exercise. If you would like to hear the instructions before starting the exercise, touch my hand.  Otherwise, touch my head to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">This exercise is called the arm to side exercise. If you would like to hear the instructions before starting the exercise, touch my hand.  Otherwise, touch my head to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/ArmToSide/Stand Back</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please stay far enough back so we both have enough space to complete the exercise.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please stay far enough back so we both have enough space to complete the exercise.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/ArmToSide/Stand Back Demo</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please stand far enough back so I have enough room to demonstrate the exercise.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please stand far enough back so I have enough room to demonstrate the exercise.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/BreakBetweenExercise/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>We are about to start our next exercise. Touch my hand when you are ready</source>
+            <comment>Text</comment>
+            <translation type="unfinished">We are about to start our next exercise. Touch my hand when you are ready</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/TotalArmStretch/Instructions</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sit straight in your chair and lean forward over your
+knees. Make sure that your arms hang loosely in a relaxed stretch. You should feel a pull through your shoulders. We will hold the stretch for a count of 10 and then slowly straighten up. Touch my hand to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sit straight in your chair and lean forward over your
+knees. Make sure that your arms hang loosely in a relaxed stretch. You should feel a pull through your shoulders. We will hold the stretch for a count of 10 and then slowly straighten up. Touch my hand to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/TotalArmStretch/Introduce Exercise</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>This exercise is called the total arm stretch. If you would like to hear the instructions before starting the exercise, touch my hand.  Otherwise, touch my head to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">This exercise is called the total arm stretch. If you would like to hear the instructions before starting the exercise, touch my hand.  Otherwise, touch my head to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/TotalArmStretch/Stand Back</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please stay far enough back so we both have enough space to complete the exercise.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please stay far enough back so we both have enough space to complete the exercise.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Exercises/TotalArmStretch/Stand Back Demo</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please stand far enough back so I have enough room to demonstrate the exercise.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please stand far enough back so I have enough room to demonstrate the exercise.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, my name is Pepper and I will be your exercise companion. Before we start, I need to advise you that if you want to stop during this rehabilitation program due to whatever reason then please do so. I want to take care of you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, my name is Pepper and I will be your exercise companion. Before we start, I need to advise you that if you want to stop during this rehabilitation program due to whatever reason then please do so. I want to take care of you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Animated Say (1)</name>
+        <message>
+            <source>Now for the fun part, picking the exercises. Each row on my tablet have exercises for you to choose. Pick 2 from each row and leave 1 blank. When you are done, touch my head.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now for the fun part, picking the exercises. Each row on my tablet have exercises for you to choose. Pick 2 from each row and leave 1 blank. When you are done, touch my head.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Now for the fun part, time to pick the exercises you want to do. Select the exercises on my tablet and then touch my hand when you are done</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Now for the fun part, time to pick the exercises you want to do. Select the exercises on my tablet and then touch my hand when you are done</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/AskForSmile</name>
+        <message>
+            <source>Ok so we are almost ready to start, but first I need to see a smile from you so that I know you are happy to go on</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ok so we are almost ready to start, but first I need to see a smile from you so that I know you are happy to go on</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/BeforeQuestion1</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Before we start I need to ask a few questions. This will be about how many of the exercises you will be able to do</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Before we start I need to ask a few questions. This will be about how many of the exercises you will be able to do</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Chair?/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you able to use a chair? Touch the back of my hand for Yes, or touch the top of my head for no</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you able to use a chair? Touch the back of my hand for Yes, or touch the top of my head for no</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Chair?/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is excellent news. This means that we can have some more fun</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is excellent news. This means that we can have some more fun</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Chair?/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Don't worry, we are still able to do a lot of things without using a chair</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Don't worry, we are still able to do a lot of things without using a chair</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/LieDown?/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is excellent news. This means that we can have some more fun</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is excellent news. This means that we can have some more fun</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/LieDown?/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Don't worry, we are still able to do a lot of things without using the floor. I can imagine it might be a bit dirty</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Don't worry, we are still able to do a lot of things without using the floor. I can imagine it might be a bit dirty</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/LieDown?/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you able to lie down on the floor? Touch the back of my hand for Yes, or touch the top of my head for no</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you able to lie down on the floor? Touch the back of my hand for Yes, or touch the top of my head for no</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Ready?/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Woohoo, I am happy that you are ready</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Woohoo, I am happy that you are ready</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Ready?/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh OK, no problem. Touch my head when you feel like you want to start</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh OK, no problem. Touch my head when you feel like you want to start</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Ready?/ReadyToStart</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you ready to start this exciting journey? Touch the back of my hand for Yes, or touch the top of my head for no</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you ready to start this exciting journey? Touch the back of my hand for Yes, or touch the top of my head for no</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Smile/NoSmile</name>
+        <message>
+            <source>I can't detect a smile which probably means that I need to buy a new pair of glasses</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I can't detect a smile which probably means that I need to buy a new pair of glasses</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Smile/YesSmile</name>
+        <message>
+            <source>Good, You have a very nice smile. I am glad you are ready to start</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good, You have a very nice smile. I am glad you are ready to start</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Table?/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is excellent news. This means that we can have some more fun</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is excellent news. This means that we can have some more fun</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Table?/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Don't worry, we are still able to do a lot of things without using a table</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Don't worry, we are still able to do a lot of things without using a table</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Intro/Table?/Table?</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you able to use a table? Touch the back of my hand for Yes, or touch the top of my head for no</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you able to use a table? Touch the back of my hand for Yes, or touch the top of my head for no</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MemoryGame/Game Instructions</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>In this game I will ask you to touch my body parts in a certain order.  You can touch my head and hands with your weaker hand, and touch my foot by tapping it with your foot.  You will have three lives, which will be displayed on my screen.  When you are out of lives, the game will end. Touch my hand to start the game, or touch my foot to quit.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">In this game I will ask you to touch my body parts in a certain order.  You can touch my head and hands with your weaker hand, and touch my foot by tapping it with your foot.  You will have three lives, which will be displayed on my screen.  When you are out of lives, the game will end. Touch my hand to start the game, or touch my foot to quit.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MemoryGame/Instructions Question</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>If you would like to hear the instructions before we begin, touch my hand.  Otherwise, touch my head to start playing.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">If you would like to hear the instructions before we begin, touch my hand.  Otherwise, touch my head to start playing.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MemoryGame/Memory Game Intro</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Would you like to play a game with me? If yes, touch my hand. If no, touch my head.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Would you like to play a game with me? If yes, touch my hand. If no, touch my head.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/MemoryGame/Quit</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alright, maybe next time.  See you soon!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alright, maybe next time.  See you soon!</translation>
+        </message>
+    </context>
+</TS>
