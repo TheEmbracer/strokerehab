467f43d8b37a9ee622e765640144c61501a736e7
TheEmbracer <39994982+TheEmbracer@users.noreply.github.com>
2018-08-02 11:55:25 +0100

    Complete version of TotalArmStretch

---
+++ TotalArmStretch/TotalArmStretch/translations/translation_en_US.ts
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Instructions</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sit straight in your chair and lean forward over your
+knees. Make sure that your arms hang loosely in a relaxed stretch. You should feel a pull through your shoulders. We will hold the stretch for a count of 10 and then slowly straighten up. Touch my hand to begin.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sit straight in your chair and lean forward over your
+knees. Make sure that your arms hang loosely in a relaxed stretch. You should feel a pull through your shoulders. We will hold the stretch for a count of 10 and then slowly straighten up. Touch my hand to begin.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Introduce Exercise</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>This exercise is called the total arm stretch.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">This exercise is called the total arm stretch.</translation>
+        </message>
+    </context>
+</TS>
